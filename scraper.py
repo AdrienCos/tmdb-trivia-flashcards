07fa5af582d2d3a147624b161381412ad1e578eb
Adrien Cosson <ad.cosson@gmail.com>
2018-04-21 22:19:12 +1000

    Parallelized the download of the posters

--- scraper.py
+++ scraper.py
@@ -3,8 +3,11 @@
 import json
 import os
 from urllib.request import urlopen
+from urllib.parse import quote
 from multiprocessing import Pool
 import argparse
+from time import sleep
+from random import random
 
 # Create the argparser
 parser = argparse.ArgumentParser()
@@ -38,9 +41,20 @@ def get_poster_url(name, api_key=api_key):
     """Given the name of a movie, returns the url of this movie's poster"""
     URL = "https://api.themoviedb.org/3/search/movie?api_key=%s&language=en-US&query=%s&page=1&include_adult=false"\
         % (api_key, name)
-    URL = URL.replace(" ", "%20")
-    print("Searching for the movie %s" % (name))
-    response = urlopen(URL)
+    # Sanitize the URL
+    URL = quote(URL, ":=&/?")
+    # Keep querying the API until we get an answer
+    res = False
+    while not res:
+        try:
+            print("Searching for the movie %s" % (name))
+            response = urlopen(URL)
+            res = True
+        except:
+            print("Error with movie %s" % name)
+            sleep(random()*10)
+    status = response.getcode()
+    # print(status)
     html = response.read()
     data = json.loads(html)
     result_movie = data["results"][0]
@@ -52,28 +66,35 @@ def download_poster(movie, api_key=api_key, base_url=base_image_url, pics_folder
     poster_path = get_poster_url(name, api_key)
     filename = poster_path.split("/")[1]
     URL = base_url + poster_path
-    img = urlopen(URL).read()
+    response = urlopen(URL)
+    status = response.getcode()
+    # print(status)
+    img = response.read()
     file = open(pics_folder + filename, "wb")
     file.write(img)
     file.close
     return filename
 
+def worker(movie, api_key=api_key, base_url=base_image_url):
+    """Worker function for parallelization of the downloads"""
+    filename = download_poster(movie, api_key, base_url)
+    name = movie["name"]
+    year = movie ["year"]
+    img_field = "<img src='%s' />\n" % filename
+    card = "%s\t%s\t%s" % (name, year, img_field)
+    return card
+
 def scrape(csv_path=csv_path, api_key=api_key, base_url=base_image_url, pics_folder=pics_folder):
     """Given the path to a CSV file, downloads every poster image related to
     the movies in the file, and creates a new CSV file suited for Anki import"""
-    output = open("cards.csv", "w")
     movies = read_csv(csv_path)
-    for movie in movies:
-        filename = download_poster(movie, api_key, base_image_url)
-        name = movie["name"]
-        year = movie["year"]
-        img_field = "<img src='%s' />\n" % filename
-        card = "%s\t%s\t%s" % (name, year, img_field)
-        print(card)
+    with Pool(10) as p:
+        cards = p.map(worker, movies)
+    output = open("cards.csv", "w")
+    for card in cards:
         output.write(card)
     output.close()
-
     return len(movies)
 
 if __name__ == '__main__':
-    print(scrape())
+    scrape()
